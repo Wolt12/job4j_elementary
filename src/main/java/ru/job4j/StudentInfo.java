25f5548882337998f6e1228250b30ca9bee353b5
ssaid <saidmatsiev1@gmail.com>
2025-07-10 19:58:57 +0300

    3.3 загрузка на GitHub

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+
+        System.out.println("Мациев Саид Баширович");
+        System.out.println("03.09.2009");
+    }
+}
